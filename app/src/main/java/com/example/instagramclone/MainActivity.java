40724e94b7ef5b6df8c6c9685d72cb5f42785e2a
vietnguyenhoangw@gmail.com <hoangviet2>
2019-09-04 16:23:44 +0700

    [ADD] - :sparkles: adding bottom nav bar

--- app/src/main/java/com/example/instagramclone/MainActivity.java
+++ app/src/main/java/com/example/instagramclone/MainActivity.java
@@ -4,11 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    TabLayout tabLayoutNavbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationBar();
+    }
+
+    public void bottomNavigationBar() {
+        tabLayoutNavbar = findViewById(R.id.tabLayout);
+        tabLayoutNavbar.addTab(tabLayoutNavbar.newTab().setIcon(R.drawable.home));
+        tabLayoutNavbar.addTab(tabLayoutNavbar.newTab().setIcon(R.drawable.search));
+        tabLayoutNavbar.addTab(tabLayoutNavbar.newTab().setIcon(R.drawable.addposts));
+        tabLayoutNavbar.addTab(tabLayoutNavbar.newTab().setIcon(R.drawable.like));
+        tabLayoutNavbar.addTab(tabLayoutNavbar.newTab().setIcon(R.drawable.user));
     }
 }
+
