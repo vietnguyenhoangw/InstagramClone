000c973ee5c161c1808de9adfe30625786d205dd
vietnguyenhoangw@gmail.com <hoangviet2>
2019-09-04 18:22:29 +0700

    [UPD] - :construction: create topbar User Fragment

--- app/src/main/java/com/example/instagramclone/Fragments/UserFragment.java
+++ app/src/main/java/com/example/instagramclone/Fragments/UserFragment.java
@@ -3,6 +3,8 @@ package com.example.instagramclone.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -10,12 +12,14 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.instagramclone.R;
+import com.google.android.material.tabs.TabLayout;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class UserFragment extends Fragment {
 
+    TabLayout tabLayoutUser;
 
     public UserFragment() {
         // Required empty public constructor
@@ -29,4 +33,13 @@ public class UserFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_user, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        tabLayoutUser = view.findViewById(R.id.userTabLayout);
+
+        tabLayoutUser.addTab(tabLayoutUser.newTab().setIcon(R.drawable.baseline_grid_on_black_48));
+        tabLayoutUser.addTab(tabLayoutUser.newTab().setIcon(R.drawable.baseline_assignment_ind_black_48));
+    }
 }
