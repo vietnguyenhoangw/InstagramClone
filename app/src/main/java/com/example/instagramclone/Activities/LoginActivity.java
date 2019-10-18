c21ab1c0296e520f3d4a7104fc722b921631c6bf
vietnguyenhoangw <vietnguyenhoangw@gmail.com>
2019-10-18 22:24:43 +0700

    [

--- app/src/main/java/com/example/instagramclone/Activities/LoginActivity.java
+++ app/src/main/java/com/example/instagramclone/Activities/LoginActivity.java
@@ -2,15 +2,31 @@ package com.example.instagramclone.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.instagramclone.MainActivity;
 import com.example.instagramclone.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnLogin = findViewById(R.id.bttLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+
+                startActivity(intent);
+            }
+        });
     }
 }
